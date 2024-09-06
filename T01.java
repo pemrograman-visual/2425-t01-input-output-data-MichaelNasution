a372414ddbf4703ef57ef95312dbd73ccffe5d3e
Michael Pratama Nasution <100076526+striktrr@users.noreply.github.com>
2024-09-06 11:19:07 +0700

    Telah Menyelesaikan Tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,27 @@
-// NIM - Your Name
+// 12S24005 - MICHAEL PRATAMA NASUTION
+// 12S24045 - MUTIARA YH SIANTURI
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, jUDUL, pENULIS, pENERBIT, formatBuku;
+        int tahunTerbit, stokBuku;
+        double hargaPembelian, minMargin, rating;
+
+        iSBN = input.nextLine();
+        jUDUL = input.nextLine();
+        pENULIS = input.nextLine();
+        tahunTerbit = input.nextInt();
+        pENERBIT = input.nextLine();
+        formatBuku = input.nextLine();
+        hargaPembelian = input.nextDouble();
+        minMargin = input.nextDouble();
+        stokBuku = input.nextInt();
+        rating = input.nextDouble();
+        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + tahunTerbit + "|" + pENERBIT + "|" + formatBuku + "|" + hargaPembelian + "|" + minMargin + "|" + stokBuku + "|" + rating);
+    }
+}
